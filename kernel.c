09478b5dfa2dd65830374dce9033bb8146d600fb
euLuann <93358525+euluann@users.noreply.github.com>
2026-09-16 12:34:44 -0300

    Atualizar o kernel.c

--- kernel.c
+++ kernel.c
@@ -5,29 +5,47 @@
 // ATENCAO //
 // O kernel.c DEVE comecar pela funcao main, ou o bootloader ira falhar
 
+ // Pre declara funcoes e variaveis declaradas apos o main, ou nao sera possivel usa-las no main, mas NAO crie uma funcao antes do main
+volatile char *video = (volatile char *)0xB8000;
+int cursor = 0;
+void print(const char *str);
+void clear(void);
+
 void kernel_main(void){
-    volatile char *video = (volatile char *)0xB8000;
-
-    int strlen(const char *str); // Pre declara uma funcao declarada apos o main, ou nao sera possivel usala no main
-
-    char *text = "Bootloader funcionando, este texto esta sendo imprimido pelo kernel em x86_64";
-
-    int text_size = strlen(text);
-
-    int i2 = 0;
-    for (int i = 0; i < text_size; i++){
-        video[i2] = text[i];
-        video[i2+1] = 0x07;
-        i2 += 2;
-    }
+    
+    //print("Este texto esta sendo imprimido pelo kernel em x86_64");
+    
+    clear();
+    print("Linha 1\n");
+    print("Linha 2\n");
+    print("Este texto esta sendo imprimido pelo kernel em x86_64");
 
     for (;;){
         __asm__ volatile ("hlt");
     }
 }
 
-int strlen(const char *str)
-{
+void print(const char *str){
+    while (*str != '\0') {
+        if (*str == '\n') {
+            cursor += 80 - (cursor % 80);
+        } else {
+            video[cursor * 2] = *str;
+            video[cursor * 2 +1] = 0x07;
+            cursor++;
+        }
+        str++;
+    }
+}
+
+void clear(void){
+    for (int i = 0; i < 80 * 25; i++){
+        video[i * 2] = ' ';
+        video[i * 2 + 1] = 0x07;
+    }
+}
+
+int strlen(const char *str){
     int i = 0;
 
     while (str[i] != '\0') {
@@ -36,3 +54,5 @@ int strlen(const char *str)
 
     return i;
 }
+
+
