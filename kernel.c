5118fb3c822408e6506d66de27280d22102f6d73
euLuann <93358525+euluann@users.noreply.github.com>
2026-09-16 06:19:00 -0300

    Atualizar o kernel.c
    Correção na indentação

--- kernel.c
+++ kernel.c
@@ -1,9 +1,6 @@
-
 // encoding: utf-8
 // Copyright (c) 2026 Luan Pestana
 // SPDX-License-Identifier: MIT
-
-// Kernel experimental
 
 // ATENCAO //
 // O kernel.c DEVE comecar pela funcao main, ou o bootloader ira falhar
@@ -13,12 +10,15 @@ void kernel_main(void){
 
     int strlen(const char *str); // Pre declara uma funcao declarada apos o main, ou nao sera possivel usala no main
 
-    char *text = "Bootloader funcionando, este texto esta sendo imprimido pelo kernel em x86_64";                                                                   
+    char *text = "Bootloader funcionando, este texto esta sendo imprimido pelo kernel em x86_64";
+
     int text_size = strlen(text);
-                                                                                      int i2 = 0;
+
+    int i2 = 0;
     for (int i = 0; i < text_size; i++){
         video[i2] = text[i];
-        video[i2+1] = 0x07;                                                               i2 += 2;
+        video[i2+1] = 0x07;
+        i2 += 2;
     }
 
     for (;;){
